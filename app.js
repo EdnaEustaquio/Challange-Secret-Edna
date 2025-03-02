b5d9a8f40e4cc341b943ec5c8d37672deb844f58
EdnaEustaquio <eustaquiosihsa@gmail.com>
2025-03-01 20:42:00 -0600

    agregue challange

---
+++ app.js
@@ -0,0 +1,10 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo (){
+
+    let nombres = document.getElementById("amigo").value;
+    amigos.push(nombres);
+    alert(amigos);
+}
